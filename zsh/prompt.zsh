125581754d70d89cf39943aa7aa480acfe18d58a
James Adam <james@lazyatom.com>
2012-02-05 12:54:03 +0000

    Must stop commiting while accidentally paired.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,50 +1,3 @@
-# git_diff_color() {
-#   changes=$(git status)
-#   case $changes in
-#     *Untracked*)
-#       echo "%{$fg[red]%}"
-#       return
-#     ;;;
-#     
-#     *updated*)
-#       echo "%{$fg[red]%}"
-#       return
-#     ;;;
-#     
-#     *committed*)
-#       echo "%{$fg[green]%}"
-#       return
-#     ;;;
-#   esac
-# }
-# 
-# git_prompt_info() {
-#   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-#   echo "($(git_diff_color)${ref#refs/heads/}$(host_prompt_color))"
-# }
-# 
-# autoload -U colors
-# colors
-# 
-# setopt prompt_subst
-# 
-# # If we're running in an ssh session, use a different colour 
-# # than if we're on a local machine
-# 
-# host_prompt_color() {
-#   case ${SSH_CLIENT} in 
-#     [0-9]*)
-#       echo "%{$fg[yellow]%}"
-#     ;;;
-#     
-#     *)
-#       echo "%{$fg[blue]%}"
-#     ;;;
-#   esac
-# }
-# 
-# export PROMPT=$'$(host_prompt_color)%n@%m:%~$(git_prompt_info)$ %{$fg[white]%}'
-
 autoload colors; colors
 autoload -U add-zsh-hook
 add-zsh-hook precmd prompt_lazyatom_precmd
@@ -87,5 +40,14 @@ function lazyatom_git_prompt_info() {
       remote="%{$fg[yellow]%}"
     ;;;
   esac
-  echo " %{$fg[yellow]%}(${branch#refs/heads/})${remote}${state}"
+  echo " %{$fg[yellow]%}(${branch#refs/heads/})${remote}${state}$(git_pair_info)"
+}
+
+git_pair_info() {
+  pair=$(git config pair.initials) || return
+  case ${pair} in
+    [a-z]*)
+      echo "%{$fg[blue]%}‣${pair}"
+    ;;;
+  esac
 }
