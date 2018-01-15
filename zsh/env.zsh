3edce4aca60793cefd4e1639f98e52b22fdf680c
James Adam <james@lazyatom.com>
2018-01-15 10:58:02 +0000

    Ensure when Chruby boots that local binaries retain precidence
    Without this, when Chruby changes the ruby, any of the
    locally-available bin files from bundler are no longer at the start of
    $PATH and so we end up running the global rake, for example.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -7,11 +7,9 @@ if [ -f ~/.gpg-agent-info ]; then
   source ~/.gpg-agent-info
 fi
 
-# RVM installer added line:
-if [ -s ~/.rvm/scripts/rvm ] ; then source ~/.rvm/scripts/rvm ; fi
-
-# Boot rbenv if it exists
-if [ -s ~/.rbenv ] ; then eval "$(rbenv init -)" ; fi
+# Boot chruby
+if [ -s /usr/local/share/chruby/chruby.sh ] ; then source '/usr/local/share/chruby/chruby.sh'; fi
+if [ -s /usr/local/share/chruby/auto.sh ] ; then source '/usr/local/share/chruby/auto.sh'; fi
 
 # Pratik's recommended ruby performance settings
 export RUBY_HEAP_MIN_SLOTS=500000
@@ -27,3 +25,17 @@ fi
 # See http://nuclearsquid.com/writings/reporttime-in-zsh/
 export REPORTTIME=10
 export FZF_DEFAULT_COMMAND='ag -g ""'
+
+save_function()
+{
+  local ORIG_FUNC="$(declare -f $1)"
+  local NEWNAME_FUNC="$2${ORIG_FUNC#$1}"
+  eval "$NEWNAME_FUNC"
+}
+
+save_function chruby old_chruby
+
+chruby() {
+  old_chruby $*
+  PATH=bin:.bundle/bin:$PATH
+}
