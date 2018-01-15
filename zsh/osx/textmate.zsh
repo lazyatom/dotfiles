9d2851af16eb7a7552bcece063c31398cde75c64
James Adam <james@lazyatom.com>
2018-01-15 11:02:13 +0000

    Remove TextMate default setting
    This slows down starting the shell quite a bit

--- zsh/osx/textmate.zsh
+++
@@ -1,4 +0,0 @@
-/usr/libexec/PlistBuddy -c 'Delete OakShellVariables' ~/Library/Preferences/com.macromates.textmate.plist
-/usr/libexec/PlistBuddy -c 'Add OakShellVariables Array' ~/Library/Preferences/com.macromates.textmate.plist
-/usr/libexec/PlistBuddy -c 'Merge /Users/james/.dotfiles/resources/textmate_environment_variables.plist OakShellVariables' ~/Library/Preferences/com.macromates.textmate.plist
-# /usr/libexec/PlistBuddy -c 'Print OakShellVariables' ~/Library/Preferences/com.macromates.textmate.plist
